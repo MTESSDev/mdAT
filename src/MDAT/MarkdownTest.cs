f9378d32f0211eab5c35255d5512f32977e0c351
anisite <infologique@gmail.com>
2023-04-01 22:43:14 -0400

    fix cariage returns linux

--- src/MDAT/MarkdownTest.cs
+++ src/MDAT/MarkdownTest.cs
@@ -145,7 +145,7 @@ namespace MDAT
 
             var summary = methodComments is { }
                             && !string.IsNullOrWhiteSpace(methodComments.Summary)
-                            ? $"\r\n\r\n> {methodComments?.Summary.Replace("\r\n", "\\\r\n")}"
+                            ? $"\r\n\r\n> {methodComments?.Summary.ReplaceLineEndings("\\\r\n")}"
                             : "";
 
             File.WriteAllText(ParsedPath, $"# {testMethod.Name}{summary}\r\n\r\n## Case 1\r\n\r\nDescription\r\n\r\n``````yaml\r\n{code}``````");
@@ -172,7 +172,7 @@ namespace MDAT
                 {
                     var def = GetDefaultValueForProperty(pi);
 
-                    string strDef = def is { } ? new Serializer().Serialize(def) : "null\r\n";
+                    string strDef = def is { } ? new Serializer().Serialize(def).ReplaceLineEndings("\r\n") : "null\r\n";
 
                     obj += $"{indent}{pi.Name}: {strDef}";
 
