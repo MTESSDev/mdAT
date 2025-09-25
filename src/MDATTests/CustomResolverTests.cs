c13876397cb7ed8a7e9413d7bff0768d2a95dbec
Gagnon, Samuel (DPNM) <Samuel.Gagnon9@mess.gouv.qc.ca>
2025-09-25 10:59:17 -0400

    Test_AddYamlTypeConverter

---
+++ src/MDATTests/CustomResolverTests.cs
@@ -0,0 +1,26 @@
+﻿using MDAT;
+using MDATTests.Resolver;
+using Microsoft.Extensions.Primitives;
+
+namespace MDATTests;
+
+[TestClass]
+public class CustomResolverTests
+{
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext testContext)
+    {
+        MdatConfig.AddYamlTypeConverter(new StringValuesTestYamlTypeConverter());
+    } 
+
+    /// <summary>
+    /// Test StringValuesTestYamlTypeConverter
+    /// </summary>
+    [TestMethod]
+    [MarkdownTest("~\\Tests\\{method}.md")]
+    public async Task Test_AddYamlTypeConverter(StringValues input, Expected expected)
+    {
+        _ = await Verify.Assert(() =>
+                                    Task.FromResult(input), expected);
+    }
+}
---
+++ src/MDATTests/Tests/test-add-yaml-type-converter.md
@@ -0,0 +1,21 @@
+﻿# Test_AddYamlTypeConverter
+
+> Test StringValuesTestYamlTypeConverter
+
+## Case 1
+
+Description
+
+``````yaml
+input: un;test
+expected:
+  name: null
+  generateExpectedData: null
+  verify: 
+    - type: match
+      jsonPath: $
+      allowAdditionalProperties: false
+      data: 
+        - un
+        - test
+``````
